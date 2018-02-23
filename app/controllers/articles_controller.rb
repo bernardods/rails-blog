e9a9710b8dc70c0be67d23878efeb43c65c7c50f
Bernardo Duarte <bernardo.duarte@goldengatetec.com.br>
2018-02-23 11:12:05 -0300

    Adding Some Validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,29 @@
 class ArticlesController < ApplicationController
 
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
   def index
      @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-
-  def create
-    @article = Article.new(article_params)
-
-    @article.save
-    redirect_to @article
   end
 
   private
